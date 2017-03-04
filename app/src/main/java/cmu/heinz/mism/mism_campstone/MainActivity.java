1bee28790907c61b167e19c7a1a2d727dc57b6b1
haokanga <haokanga@andrew.cmu.edu>
2017-03-04 15:17:50 -0500

    git init

---
+++ app/src/main/java/cmu/heinz/mism/mism_campstone/MainActivity.java
@@ -0,0 +1,13 @@
+package cmu.heinz.mism.mism_campstone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
